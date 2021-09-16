e67c157bdc9411cf3b66920d488aa8d54c913c7b
Stefan <stefan.wezel@student.uni-tuebingen.de>
2021-09-16 21:49:55 +0200

    rename

--- slides/utils.py
+++ slides/utils.py
@@ -24,6 +24,7 @@ COLORS = dict(
     linreg = '#519872',
     abc = 'purple',
     sbi  = '#DE1A1A',
+    model= '#DE1A1A',
     )
 
 
@@ -48,19 +49,20 @@ def set_plot_attributes(ax, xlabel='',ylabel='',title='',xlim=None,ylim=None,leg
 
 def interactive_projectile(simulator):
     """ Returns an interactive plot for the projectile simulation using a wrapper. """
-    def visualize_projectile(launch_velocity, launch_angle, drag_coefficient):
+    def visualize_projectile(speed, angle, drag):
+        area=0.008 # this is here or it will show up in the interactive plot
+        mass=0.2 # same here
         # perform simulation using arguments from interactive plot
-        θ = launch_velocity, launch_angle, drag_coefficient
-        simulation  = simulator(θ)
+        simulation  = simulator(speed, angle, drag, area, mass)
         # plotting
         fig, ax = plt.subplots()
-        ax.plot(simulation['distance_travelled'], simulation['height'],label='Ground truth trajetory', color=COLORS['ground_truth'])
-        ax.scatter(simulation['distance_travelled'], simulation['x'],label='x_o', marker='x', color=COLORS['data'])
+        ax.plot(simulation['d'], simulation['h'],label='Ground truth trajectory', color=COLORS['ground_truth'])
+        ax.scatter(simulation['d'], simulation['x'],label='x_o', marker='x', color=COLORS['data'])
         set_plot_attributes(ax, xlim=(0,350), ylim=(0,150), xlabel='distance traveled (m)', ylabel='height (m)', legend=True)
         
-        return dict(θ=θ, distance_travelled=simulation['distance_travelled'], height=simulation['height'], x=simulation['x'])
+        return dict(θ=(speed,angle,drag,area,mass), d=simulation['d'], h=simulation['h'], x=simulation['x'])
     
-    return interactive(visualize_projectile, launch_velocity=(50,250), launch_angle=(10,45), drag_coefficient=(0.2,0.7,0.01))
+    return interactive(visualize_projectile, speed=(50,250), angle=(10,45), drag=(0.2,0.7,0.01))
 
 
 
@@ -92,3 +94,16 @@ def interactive_pendulum(simulator):
         return dict(θ=θ, x_o=x_o)
 
     return interactive(visualize_pendulum, dampening_factor=(0,1,0.1), mass=(0.01,5,0.1), length=(1,4,0.5))
+
+
+def plot_data_vs_model(domain,
+                       data,
+                       model,
+                       data_label='data',
+                       model_label='prediction',
+                       plot_attrs=dict(legend=True, ylim=(0, 250), xlabel='distance traveled (m)', ylabel='height (m)')
+                      ):
+    fig, ax = plt.subplots()
+    ax.scatter(domain, data, label=data_label, marker='x', color=COLORS['data'])
+    ax.plot(domain, model, label = model_label, color=COLORS['sbi'])
+    set_plot_attributes(ax, **plot_attrs)
